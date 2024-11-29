cfd9924751049e7648aac6f0ec0a9e82e2d8d4f2
BemMeQuer30 <mequerbem030@gmail.com>
2024-11-29 14:34:24 -0300

    dadad

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthService from "../../services/AuthService";
+import ReCAPTCHA from "react-google-recaptcha";
 import "./styles.css";
 
 const authService = new AuthService();
 
 const Login: React.FC = () => {
-  const [isMounted, setIsMounted] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState("");
-  const navigation = useNavigate();
+  const [recaptchaToken, setRecaptchaToken] = useState<string | null>(null);
+
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     document.title = "Login - BMQ";
   }, []);
 
-  useEffect(() => {
-    setIsMounted(true);
-    return () => {
-      setIsMounted(false);
-    };
-  }, []);
+  const handleRecaptchaChange = (token: string | null) => {
+    setRecaptchaToken(token);
+  };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setIsSubmitting(true);
+    setError("");
+
+    // Verifica se o reCAPTCHA foi resolvido
+    if (!recaptchaToken) {
+      setError("Por favor, complete o reCAPTCHA.");
+      setIsSubmitting(false);
+      return;
+    }
 
     try {
       const response = await authService.login(email, password);
       if (response.access_token) {
-        if (isMounted) {
-          setIsSubmitting(false);
-        }
-        console.log(response);
-        console.log(response.user.role === "ADMIN");
         if (response.user.role === "ADMIN") {
-          console.log();
-          navigation("/admin/dashboard");
-        } else navigation("/");
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/");
+        }
       }
-    } catch {
-      console.error(error);
-      if (isMounted) {
-        setError("Email ou senha inválidos");
-      }
-    }
-
-    if (isMounted) {
+    } catch (err) {
+      console.error(err);
+      setError("Email ou senha inválidos.");
+    } finally {
       setIsSubmitting(false);
     }
   };
@@ -90,6 +90,11 @@ const Login: React.FC = () => {
                 />
               </div>
               {error && <p className="error-message">{error}</p>}
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6Ld7zo0qAAAAANw2VatZhTs-AaNnRKqgxpcaP9Cj"
+                onChange={handleRecaptchaChange}
+              />
               <button
                 type="submit"
                 className="login-button"
